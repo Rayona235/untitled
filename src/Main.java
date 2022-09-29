f879def059fe3baa9fa0829981c88fcc3d8f35fe
Acer <atabekovaayona@gmail.com>
2022-09-29 15:35:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        String nameRayona;
+        System.out.println();
+        final int NUM=567;
+        String word="WORD";
+        System.out.println( word);
+        System.out.println( "nameRayona" + "NUM "+ "WORD");
+
+
+        int num=567;
+          if (num > 0){
+              System.out.println("Вы сохранили положителное число ");
+          }else if (num < 0){
+              System.out.println("ВЫ сохранили отрицательное число");
+          }else {
+              System.out.println("ВЫ сохранили нуль");
+          }
+    }
+
+}
